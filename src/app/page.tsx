4f286c7e5ced13a28074f849dbea56c5880b88f5
sijun <sionwer5@gmail.com>
2025-08-02 17:37:07 +0900

    feat: list 레이아웃 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,46 +3,69 @@ import Link from "next/link";
 
 const links = [
   {
-    title: "구글 드라이브 폴더",
-    href: "https://drive.google.com/drive/folders/abc123",
-    image: "/images/google-drive.png",
+    title: "1BET1",
+    href: "https://1bet1.one/?regcode=TV82",
+    image: "/images/원벳원-400x120-TV82.jpg",
   },
   {
-    title: "PPT 템플릿",
-    href: "https://docs.google.com/presentation/d/xyz456",
-    image: "/images/ppt.png",
+    title: "ttwins",
+    href: "https://tw-aaa.com/?code=82TV",
+    image: "/images/트윈스-400120.jpg",
   },
   {
-    title: "엑셀 보고서",
-    href: "https://docs.google.com/spreadsheets/def789",
-    image: "/images/excel.png",
+    title: "로이더뱃",
+    href: "https://rd-365.com/login.asp",
+    image: "/images/로이더벳_400x120.jpg",
   },
 ];
 
 export default function Home() {
   return (
-      <main className="min-h-screen bg-white py-10 px-4">
+      <main className="min-h-screen py-10 px-4">
         <h1 className="text-3xl font-bold text-center mb-10">자료 링크 모음</h1>
 
-        <div className="grid gap-6 grid-cols-2">
-          {links.map((item, index) => (
-              <Link
-                  key={index}
-                  href={item.href}
-                  target="_blank"
-                  className="flex flex-col items-center border rounded-lg p-4 shadow hover:shadow-lg transition duration-200"
-              >
-                <div className="w-24 h-24 relative mb-4">
+        {/* 반응형 컨테이너 */}
+        <div className="max-w-6xl mx-auto">
+
+          {/* 데스크톱: 2열 Flexbox */}
+          <div className="hidden md:flex md:flex-wrap md:justify-center">
+            {links.map((item, index) => (
+                <Link
+                    key={index}
+                    href={item.href}
+                    target="_blank"
+                    className="inline-block border"
+                >
                   <Image
                       src={item.image}
                       alt={item.title}
-                      fill
-                      className="object-contain"
+                      width={400}
+                      height={120}
+                      className="max-w-[400px] h-auto"
                   />
-                </div>
-                <span className="text-lg font-medium text-center">{item.title}</span>
-              </Link>
-          ))}
+                </Link>
+            ))}
+          </div>
+
+          {/* 모바일: 1열 중앙 정렬 */}
+          <div className="md:hidden flex flex-col items-center">
+            {links.map((item, index) => (
+                <Link
+                    key={index}
+                    href={item.href}
+                    target="_blank"
+                    className="inline-block border"
+                >
+                  <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={400}
+                      height={120}
+                      className="max-w-[400px] h-auto"
+                  />
+                </Link>
+            ))}
+          </div>
         </div>
       </main>
   );
