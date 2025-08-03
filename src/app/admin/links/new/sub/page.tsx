9b956163f65f8c7e4eb01dd517cf19cb4dcec0f0
sijun <sionwer5@gmail.com>
2025-08-03 21:29:28 +0900

    feat: 링크 추가 페이지 디자인 변경

--- src/app/admin/links/new/sub/page.tsx
+++ src/app/admin/links/new/sub/page.tsx
@@ -44,33 +44,58 @@ export default function AddSubLinkPage() {
             <h1 className="text-2xl font-bold">서브 링크 추가</h1>
 
             <div className="space-y-4">
-                <label className="block text-sm font-medium">링크 주소</label>
-                <input
-                    type="text"
-                    className="border p-2 w-full"
-                    placeholder="https://example.com"
-                    value={newItem.href}
-                    onChange={(e) => setNewItem({ ...newItem, href: e.target.value })}
-                />
+                <div className="flex justify-end">
+                    <button
+                        onClick={handleSubmit}
+                        disabled={uploading}
+                        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-400 hover:cursor-pointer disabled:opacity-50"
+                    >
+                        저장하고 돌아가기
+                    </button>
+                </div>
+                <div className="space-y-2">
+                    <label className="block text-md font-medium">링크 주소</label>
+                    <input
+                        type="text"
+                        className="border p-2 w-full"
+                        placeholder="https://example.com"
+                        value={newItem.href}
+                        onChange={(e) => setNewItem({ ...newItem, href: e.target.value })}
+                    />
+                </div>
 
-                <label className="block text-sm font-medium">제목</label>
-                <input
-                    type="text"
-                    className="border p-2 w-full"
-                    placeholder="제목"
-                    value={newItem.title}
-                    onChange={(e) => setNewItem({ ...newItem, title: e.target.value })}
-                />
+                <div className="space-y-2">
+                    <label className="block text-md font-medium">제목</label>
+                    <input
+                        type="text"
+                        className="border p-2 w-full"
+                        placeholder="제목"
+                        value={newItem.title}
+                        onChange={(e) => setNewItem({ ...newItem, title: e.target.value })}
+                    />
+                </div>
 
-                <label className="block text-sm font-medium">이미지 업로드</label>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                        const file = e.target.files?.[0];
-                        if (file) handleUploadImage(file);
-                    }}
-                />
+                <div>
+                    {/* 숨겨진 실제 파일 입력 */}
+                    <input
+                        id="fileUpload"
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => {
+                            const file = e.target.files?.[0];
+                            if (file) handleUploadImage(file);
+                        }}
+                        className="hidden"
+                    />
+
+                    {/* 빨간색 버튼처럼 보이도록 label 사용 */}
+                    <label
+                        htmlFor="fileUpload"
+                        className="inline-block bg-blue-600 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-400 transition"
+                    >
+                        이미지 업로드
+                    </label>
+                </div>
 
                 {uploading && <p className="text-gray-500 text-sm">업로드 중...</p>}
 
@@ -81,14 +106,6 @@ export default function AddSubLinkPage() {
                         className="w-full max-w-xs object-contain border rounded"
                     />
                 )}
-
-                <button
-                    onClick={handleSubmit}
-                    disabled={uploading}
-                    className="bg-blue-600 text-white px-4 py-2 rounded disabled:opacity-50"
-                >
-                    저장하고 돌아가기
-                </button>
             </div>
         </div>
     );
