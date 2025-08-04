d0cf79bf69e103d7b2bbd20a354fb097cd82e30d
sijun <sionwer5@gmail.com>
2025-08-04 21:36:12 +0900

    feat: admin layout에 guard 적용

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,11 @@
 "use client";
 
-export default function AdminLayout() {
+import AdminGuard from "@/auth/guard";
 
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <AdminGuard>
+            {children}
+        </AdminGuard>
+    );
 }
