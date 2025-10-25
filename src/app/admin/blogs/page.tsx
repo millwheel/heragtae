10dadbb43cef3a2a0a7c044446a0426e4cb7613a
sijun <sionwer5@gmail.com>
2025-10-25 15:07:30 +0900

    feat: admin page 에서 링크, 블로그 관리 버튼 분리

---
+++ src/app/admin/blogs/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AdminBlogPage() {
+    return (
+        <div className="max-w-4xl mx-auto p-6 space-y-6">
+            <h1 className="text-2xl font-bold">블로그 관리</h1>
+        </div>
+    )
+}
